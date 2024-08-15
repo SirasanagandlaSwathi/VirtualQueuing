cf717489f6b612a5c1a64dd1f35b03a218706d32
SirasanagandlaSwathi <158811168+SirasanagandlaSwathi@users.noreply.github.com>
2024-08-15 10:42:11 +0530

    Add files via upload

---
+++ dams/includes/header.php
@@ -0,0 +1,47 @@
+<nav class="navbar navbar-expand-lg bg-light fixed-top shadow-lg">
+                <div class="container">
+                    <a class="navbar-brand mx-auto d-lg-none" href="index.php">
+                        RATIONSHOP SLOT BOOKING
+                        <strong class="d-block">MANAGEMENT SYSTEM</strong>
+                    </a>
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav mx-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="index.php">Home</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#about">About</a>
+                            </li>
+
+                           
+
+                            <a class="navbar-brand d-none d-lg-block" href="index.php">
+                                RATIONSHOP SLOT BOOKING
+                                <strong class="d-block">MANAGEMENT SYSTEM</strong>
+                            </a>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="check-appointment.php">Check Slot Booking</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#booking">Slot Booking</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#contact">Contact</a>
+                            </li>
+                             <li class="nav-item active">
+                                <a class="nav-link" href="doctor/login.php">Shop</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </nav>
