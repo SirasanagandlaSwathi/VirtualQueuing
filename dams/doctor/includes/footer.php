3f85359948ca3f31131d4d78f41527fd1a33019b
SirasanagandlaSwathi <158811168+SirasanagandlaSwathi@users.noreply.github.com>
2024-08-15 10:44:53 +0530

    Add files via upload

---
+++ dams/doctor/includes/footer.php
@@ -0,0 +1,8 @@
+<div class="wrap p-t-0">
+    <footer class="app-footer">
+      
+        <div class="copyright pull-left">
+		RATIONSHOP SLOT BOOKING MANAGEMENT SYSTEM </div>
+   
+    </footer>
+  </div>
